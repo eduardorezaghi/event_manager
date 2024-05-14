7a09bb5bb72fb9ea66b3a9049f35a8ca41456777
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-05-14 08:38:20 -0300

    Use CSV module to parse csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,13 @@
+require 'csv'
 puts 'Event Manager Initialized!'
+
+unless File.exist?('event_attendees.csv')
+  puts 'event_attendees.csv not found'
+end
+
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
